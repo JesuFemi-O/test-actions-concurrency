836e90835992c3b651865923d29115fc845e3943
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-08-14 23:21:21 +0100

    interrupt v10

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ signal.signal(signal.SIGINT, handle_signal)
 def main():
     print("doing some work!")
     start_time = time.time()
+    
     while time.time() - start_time < 27:
         print("Monitoring...")
         time.sleep(1)  # Add a small delay to prevent excessive looping - check in
+    print("done...")
 
 if __name__ == "__main__":
     main()
