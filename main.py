e8abeba4c0a58adba505ec83887aeb82772145dc
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-08-14 17:09:50 +0100

    add script

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+def main():
+    print("Sleeping for 3 minutes...")
+    time.sleep(180)  # Sleep for 3 minutes (180 seconds)
+    print("Done sleeping!")
+
+if __name__ == "__main__":
+    main()
